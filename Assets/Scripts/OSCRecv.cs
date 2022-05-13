d8966ec3d895aaceff92f1a7a7b8145e4c0d9f25
jaysonh <mail@jaysonh.com>
2022-05-13 14:56:32 +0800

    added 3d camera, added front plate to lights

--- Assets/Scripts/OSCRecv.cs
+++ Assets/Scripts/OSCRecv.cs
@@ -9,31 +9,33 @@ public class OSCRecv : MonoBehaviour
         // Start is called before the first frame update
         void Start()
         {
-            GameObject go = GameObject.Find("Main Camera");
+            GameObject go = GameObject.Find("OSCHandler");
             oscHandler = go.GetComponent<OSCHandler>();
 
             GameObject child = gameObject.transform.GetChild(0).gameObject;
-            light = gameObject.transform.GetChild(1).gameObject.GetComponent<Light>();
+            lightOutput      = gameObject.transform.GetChild(1).gameObject.GetComponent<Light>();
+            facePlateLight   = gameObject.transform.GetChild(2).gameObject.GetComponent<Light>();
     }
 
         // Update is called once per frame
         void Update()
         {            
-            if ( oscHandler != null && dmxChannel > -1 && light !=null )
+            if ( oscHandler != null && dmxAddress > -1 && lightOutput != null )
             {
-                int r = oscHandler.dmxVals[ dmxChannel     ];
-                int g = oscHandler.dmxVals[ dmxChannel + 1 ];
-                int b = oscHandler.dmxVals[ dmxChannel + 2 ];
+                int r = oscHandler.dmxVals[ dmxAddress     ];
+                int g = oscHandler.dmxVals[ dmxAddress + 1 ];
+                int b = oscHandler.dmxVals[ dmxAddress + 2 ];
 
-                light.color = new Color((float)r/255.0f, (float)g / 255.0f, (float)b / 255.0f, 1.0f);
+                lightOutput.color    = new Color((float)r / 255.0f, (float)g / 255.0f, (float)b / 255.0f, 1.0f);
+                facePlateLight.color = new Color((float)r / 255.0f, (float)g / 255.0f, (float)b / 255.0f, 1.0f);
 
 
-                // set light based on channel
+            // set light based on channel
             }
         }
         OSCHandler oscHandler;
-        Light light;
+        Light lightOutput;
+        Light facePlateLight;
 
-
-        public int dmxChannel = -1;
+        public int dmxAddress = -1;
 }
